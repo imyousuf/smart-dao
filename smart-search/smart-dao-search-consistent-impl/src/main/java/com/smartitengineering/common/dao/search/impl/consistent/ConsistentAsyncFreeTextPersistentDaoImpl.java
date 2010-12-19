139e9561b7ff78660e5767f1fd706fbaadadf162
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-12-19 13:33:07 +0600

    Implement addition and deletion from persistent storage
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- smart-search/smart-dao-search-consistent-impl/src/main/java/com/smartitengineering/common/dao/search/impl/consistent/ConsistentAsyncFreeTextPersistentDaoImpl.java
+++ smart-search/smart-dao-search-consistent-impl/src/main/java/com/smartitengineering/common/dao/search/impl/consistent/ConsistentAsyncFreeTextPersistentDaoImpl.java
@@ -19,56 +19,170 @@
 package com.smartitengineering.common.dao.search.impl.consistent;
 
 import com.google.inject.Inject;
+import com.google.inject.internal.Nullable;
 import com.google.inject.name.Named;
 import com.smartitengineering.common.dao.search.CommonFreeTextPersistentDao;
 import com.smartitengineering.common.dao.search.impl.CommonAsyncFreeTextPersistentDaoImpl;
 import com.smartitengineering.dao.common.CommonReadDao;
 import com.smartitengineering.dao.common.CommonWriteDao;
 import com.smartitengineering.domain.PersistentDTO;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author imyousuf
  */
-public class ConsistentAsyncFreeTextPersistentDaoImpl<T extends PersistentDTO> extends CommonAsyncFreeTextPersistentDaoImpl<T> {
+public class ConsistentAsyncFreeTextPersistentDaoImpl<I extends Comparable<I> & Serializable, V extends Comparable<V> & Serializable, T extends PersistentDTO<T, I, V>>
+    extends CommonAsyncFreeTextPersistentDaoImpl<T> {
 
-  @Inject
-  @Named("appName")
-  private String appName;
-  @Inject
-  @Named("agentName")
-  private String agentName;
-  @Inject
-  private CommonWriteDao<IndexableObject> writeDao;
-  @Inject
-  private CommonReadDao<IndexableObject, IndexableObjectId> readDao;
+  public static final String DELETE = "delete";
+  public static final String SAVE = "save";
+  public static final String UPDATE = "update";
+  private final String appName;
+  private final String agentName;
+  private final CommonWriteDao<IndexableObject> writeDao;
+  private final CommonReadDao<IndexableObject, IndexableObjectId> readDao;
+  private final IdToStringProvider<I> toStringProvider;
 
   @Inject
   public ConsistentAsyncFreeTextPersistentDaoImpl(@Named("saveInterval") long saveInterval,
                                                   @Named("updateInterval") long updateInterval,
                                                   @Named("deleteInterval") long deleteInterval,
                                                   @Named("intervalTimeUnit") TimeUnit timeUnit,
-                                                  @Named("primaryFreeTextPersistentDao") CommonFreeTextPersistentDao<T> primaryDao) {
-    super(saveInterval, updateInterval, deleteInterval, timeUnit, primaryDao);
+                                                  @Named("agentName") String agentName,
+                                                  @Named("appName") String appName,
+                                                  CommonWriteDao<IndexableObject> writeDao,
+                                                  CommonReadDao<IndexableObject, IndexableObjectId> readDao,
+                                                  @Named("primaryFreeTextPersistentDao") CommonFreeTextPersistentDao<T> primaryDao,
+                                                  @Nullable IdToStringProvider<I> toStringProvider) {
+    super(saveInterval, updateInterval, deleteInterval, timeUnit, new WrapperDao<T>(primaryDao, appName, agentName,
+                                                                                    writeDao, toStringProvider));
+    this.writeDao = writeDao;
+    this.readDao = readDao;
+    this.appName = appName;
+    this.agentName = agentName;
+    if (toStringProvider == null) {
+      this.toStringProvider = new DefaultIdToStringProvider<I>();
+    }
+    else {
+      this.toStringProvider = toStringProvider;
+    }
     //TODO Check persistent storage for un syncd data
+  }
+
+  protected void saveBeans(String opName, T... data) {
+    if (data == null || data.length <= 0) {
+      return;
+    }
+    List<IndexableObject> objects = new ArrayList<IndexableObject>(data.length);
+    for (T datum : data) {
+      IndexableObject object = new IndexableObject();
+      object.setSerializable(datum);
+      object.setOperationName(opName);
+      IndexableObjectId objectId = new IndexableObjectId();
+      objectId.setAgentName(agentName);
+      objectId.setAppName(appName);
+      objectId.setClassName(object.getClazzName());
+      objectId.setObjectId(toStringProvider.getToString(datum.getId()));
+      objectId.setOpName(opName);
+      object.setId(objectId);
+      objects.add(object);
+    }
+    try {
+      writeDao.save(objects.toArray(new IndexableObject[objects.size()]));
+    }
+    catch (Exception ex) {
+      logger.error("Saving data not successful!", ex);
+    }
   }
 
   @Override
   protected void deleteOps(T[] data) {
-    //TODO Save first
+    saveBeans(DELETE, data);
     super.deleteOps(data);
   }
 
   @Override
   protected void saveOps(T... data) {
-    //TODO Save first
+    saveBeans(UPDATE, data);
     super.saveOps(data);
   }
 
   @Override
   protected void updateOps(T[] data) {
-    //TODO Save first
+    saveBeans(SAVE, data);
     super.updateOps(data);
+  }
+
+  protected static class WrapperDao<T extends PersistentDTO> implements CommonFreeTextPersistentDao<T> {
+
+    private final CommonFreeTextPersistentDao<T> persistentDao;
+    private final String appName;
+    private final String agentName;
+    private final CommonWriteDao<IndexableObject> writeDao;
+    private final IdToStringProvider toStringProvider;
+    protected final transient Logger logger = LoggerFactory.getLogger(getClass());
+
+    public WrapperDao(CommonFreeTextPersistentDao<T> persistentDao, String appName, String agentName,
+                      CommonWriteDao<IndexableObject> writeDao, IdToStringProvider toStringProvider) {
+      this.persistentDao = persistentDao;
+      this.appName = appName;
+      this.agentName = agentName;
+      this.writeDao = writeDao;
+      if (toStringProvider == null) {
+        this.toStringProvider = new DefaultIdToStringProvider();
+      }
+      else {
+        this.toStringProvider = toStringProvider;
+      }
+    }
+
+    @Override
+    public void save(T... data) {
+      persistentDao.save(data);
+      deleteBeans(SAVE, data);
+    }
+
+    @Override
+    public void update(T... data) {
+      persistentDao.update(data);
+      deleteBeans(UPDATE, data);
+    }
+
+    @Override
+    public void delete(T... data) {
+      persistentDao.delete(data);
+      deleteBeans(DELETE, data);
+    }
+
+    protected void deleteBeans(String opName, T... data) {
+      if (data == null || data.length <= 0) {
+        return;
+      }
+      List<IndexableObject> objects = new ArrayList<IndexableObject>(data.length);
+      for (T datum : data) {
+        IndexableObject object = new IndexableObject();
+        object.setSerializable(datum);
+        object.setOperationName(opName);
+        IndexableObjectId objectId = new IndexableObjectId();
+        objectId.setAgentName(agentName);
+        objectId.setAppName(appName);
+        objectId.setClassName(object.getClazzName());
+        objectId.setObjectId(toStringProvider.getToString(datum.getId()));
+        object.setId(objectId);
+        objects.add(object);
+      }
+      try {
+        writeDao.delete(objects.toArray(new IndexableObject[objects.size()]));
+      }
+      catch (Exception ex) {
+        logger.error("Deleting data not successful!", ex);
+      }
+    }
   }
 }
